935cd48d2cd7c4db5b4a46313d48bbb29fd12dcd
uhl1k <jankuroman97@gmail.com>
2023-08-30 13:34:15 +0200

    created data services

--- backend/src/main/java/com/uhl1k/cvut/swa/swaappbe/services/PetrDataSource.java
+++ backend/src/main/java/com/uhl1k/cvut/swa/swaappbe/services/PetrDataSource.java
@@ -1,8 +1,11 @@
 package com.uhl1k.cvut.swa.swaappbe.services;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @FeignClient(name = "PetrService")
@@ -10,4 +13,48 @@ public interface PetrDataSource {
   @RequestMapping(path = "/petr", method = RequestMethod.GET)
   @ResponseBody
   String aliveEndPoint();
+
+  @RequestMapping(path = "/books", method = RequestMethod.GET)
+  @ResponseBody
+  Object getBooks(@RequestParam("page") int page, @RequestParam("pageSize") int pageSize, @RequestParam("title") String title);
+
+  @RequestMapping(path = "/books", method = RequestMethod.PUT)
+  @ResponseBody
+  int addBook(@RequestBody Object book);
+
+  @RequestMapping(path = "/books/{isbn}", method = RequestMethod.GET)
+  @ResponseBody
+  Object getBook(@PathVariable("isbn") String isbn);
+
+  @RequestMapping(path = "/books/{isbn}", method = RequestMethod.POST)
+  @ResponseBody
+  Object modifyBook(@PathVariable("isbn") String isbn, @RequestBody Object book);
+
+  @RequestMapping(path = "/books/{isbn}", method = RequestMethod.DELETE)
+  @ResponseBody
+  void deleteBook(@PathVariable("isbn") String isbn);
+
+  @RequestMapping(path = "/authors", method = RequestMethod.GET)
+  @ResponseBody
+  Object getAuthors(@RequestParam("page") int page, @RequestParam("pageSize") int pageSize);
+
+  @RequestMapping(path = "/authors", method = RequestMethod.PUT)
+  @ResponseBody
+  int addAuthor(@RequestBody Object author);
+
+  @RequestMapping(path = "/authors/{author-id}", method = RequestMethod.GET)
+  @ResponseBody
+  Object getAuthor(@PathVariable("author-id") int id);
+
+  @RequestMapping(path = "/authors/{author-id}", method = RequestMethod.GET)
+  @ResponseBody
+  void modifyAuthor(@PathVariable("author-id") int id, @RequestBody Object author);
+
+  @RequestMapping(path = "/authors/{author-id}", method = RequestMethod.GET)
+  @ResponseBody
+  void deleteAuthor(@PathVariable("author-id") int id);
+
+  @RequestMapping(path = "/authors/{author-id}/books", method = RequestMethod.GET)
+  @ResponseBody
+  Object getBooksFromAuthor(@PathVariable("author-id") int id);
 }
--- backend/src/main/java/com/uhl1k/cvut/swa/swaappbe/services/StepanDataSource.java
+++ backend/src/main/java/com/uhl1k/cvut/swa/swaappbe/services/StepanDataSource.java
@@ -1,13 +1,52 @@
 package com.uhl1k.cvut.swa.swaappbe.services;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @FeignClient(name = "StepanService")
 public interface StepanDataSource {
-  @RequestMapping(path = "/stepan", method = RequestMethod.GET)
+  @RequestMapping(path = "/openapi/health", method = RequestMethod.GET)
   @ResponseBody
   String aliveEndPoint();
+
+  @RequestMapping(path = "/openapi/shops", method = RequestMethod.GET)
+  @ResponseBody
+  String getShops(@RequestParam("page") int page, @RequestParam("pageSize") int pageSize);
+
+  @RequestMapping(path = "/openapi/shops", method = RequestMethod.PUT)
+  @ResponseBody
+  int addShop(@RequestBody Object shop);
+
+  @RequestMapping(path = "/openapi/shops/{id}", method = RequestMethod.GET)
+  @ResponseBody
+  Object getShop(@PathVariable(name = "id") int id);
+
+  @RequestMapping(path = "/openapi/shops/{id}", method = RequestMethod.POST)
+  @ResponseBody
+  Object modifyShop(@PathVariable(name = "id") int id, @RequestBody Object shop);
+
+  @RequestMapping(path = "/openapi/shops/{id}", method = RequestMethod.DELETE)
+  @ResponseBody
+  void deleteShop(@PathVariable(name = "id") int id);
+
+  @RequestMapping(path = "/books/{isbn}", method = RequestMethod.GET)
+  @ResponseBody
+  Object getAvailability(@PathVariable(name = "isbn") String isbn, @RequestParam("page") int page, @RequestParam("pageSize") int pageSize);
+
+  @RequestMapping(path = "/books/{isbn}/availability/{shopId}", method = RequestMethod.PUT)
+  @ResponseBody
+  void addAvailability(@PathVariable(name = "isbn") String isbn, @PathVariable(name = "shopId") int shopId);
+
+  @RequestMapping(path = "/books/{isbn}/availability/{shopId}", method = RequestMethod.POST)
+  @ResponseBody
+  void modifyAvailability(@PathVariable(name = "isbn") String isbn, @PathVariable(name = "shopId") int shopId);
+
+  @RequestMapping(path = "/books/{isbn}/availability/{shopId}", method = RequestMethod.DELETE)
+  @ResponseBody
+  void removeAvailability(@PathVariable(name = "isbn") String isbn, @PathVariable(name = "shopId") int shopId);
 }
