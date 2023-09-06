a7b540c5556a64fa0cc345f4e2ad80b0b9771b4d
uhl1k <jankuroman97@gmail.com>
2023-09-06 21:03:28 +0200

    trying feign exception handling

--- backend/src/main/java/com/uhl1k/cvut/swa/swaappbe/controller/RestExceptionHandler.java
+++ backend/src/main/java/com/uhl1k/cvut/swa/swaappbe/controller/RestExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.uhl1k.cvut.swa.swaappbe.controller;
 
+import feign.FeignException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +10,15 @@ import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class RestExceptionHandler {
+  @ExceptionHandler(value = {FeignException.class})
+  public ResponseEntity<String> feignExceptionHandler(Exception ex, WebRequest req) {
+    switch (ex.getMessage().split(":")[1].substring(2, 5)) {
+      case "400":
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+    return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
   @ExceptionHandler(value = {Exception.class})
   public ResponseEntity<String> genericErrorHandler(Exception ex, WebRequest req) {
     System.out.println(ex.getMessage());
--- backend/src/main/java/com/uhl1k/cvut/swa/swaappbe/controller/ShopsController.java
+++ backend/src/main/java/com/uhl1k/cvut/swa/swaappbe/controller/ShopsController.java
@@ -8,6 +8,7 @@ import feign.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class ShopsController {
 
