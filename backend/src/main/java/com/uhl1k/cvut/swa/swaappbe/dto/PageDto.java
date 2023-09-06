18ce0dc209f2a9c97b25428aeaf60f71cc2cacc3
uhl1k <jankuroman97@gmail.com>
2023-09-06 18:32:13 +0200

    added shops

---
+++ backend/src/main/java/com/uhl1k/cvut/swa/swaappbe/dto/PageDto.java
@@ -0,0 +1,19 @@
+package com.uhl1k.cvut.swa.swaappbe.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageDto<K> {
+  int page;
+  int pageSize;
+  int totalPages;
+
+  List<K> data;
+}
