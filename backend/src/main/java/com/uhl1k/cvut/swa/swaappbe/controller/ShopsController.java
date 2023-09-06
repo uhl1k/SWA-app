5a1de846896c0482e3ab5fd309ebb95f78a2d177
uhl1k <jankuroman97@gmail.com>
2023-09-06 21:32:38 +0200

    fix

--- backend/src/main/java/com/uhl1k/cvut/swa/swaappbe/controller/ShopsController.java
+++ backend/src/main/java/com/uhl1k/cvut/swa/swaappbe/controller/ShopsController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,7 +40,7 @@ public class ShopsController {
   }
 
   @GetMapping("/api/shops/{id}")
-  public ResponseEntity<ShopDto> getShop(@RequestParam int id) {
+  public ResponseEntity<ShopDto> getShop(@PathVariable int id) {
     return new ResponseEntity<>(stepanService.getShop(id), HttpStatus.OK);
   }
 
