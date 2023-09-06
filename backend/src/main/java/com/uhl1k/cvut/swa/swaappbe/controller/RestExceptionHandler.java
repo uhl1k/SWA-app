a784420b062bcbdcb57d378b6bd0d65a6d908571
uhl1k <jankuroman97@gmail.com>
2023-09-06 21:05:14 +0200

    another try

--- backend/src/main/java/com/uhl1k/cvut/swa/swaappbe/controller/RestExceptionHandler.java
+++ backend/src/main/java/com/uhl1k/cvut/swa/swaappbe/controller/RestExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.web.context.request.WebRequest;
 public class RestExceptionHandler {
   @ExceptionHandler(value = {FeignException.class})
   public ResponseEntity<String> feignExceptionHandler(Exception ex, WebRequest req) {
+    System.out.println(ex.getMessage().split(":")[1].substring(2, 5));
     switch (ex.getMessage().split(":")[1].substring(2, 5)) {
       case "400":
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
